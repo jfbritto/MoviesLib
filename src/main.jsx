00910655985841069ad691018bd3a9e9589901eb
João Filipi <jf.britto@hotmail.com>
2022-12-07 00:26:08 -0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import App from './App'
-import Home from './pages/Home'
-import Movie from './pages/Movie'
-import Search from './pages/Search'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./App";
+import Home from "./pages/Home";
+import Movie from "./pages/Movie";
+import Search from "./pages/Search";
 
-import './index.css'
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
